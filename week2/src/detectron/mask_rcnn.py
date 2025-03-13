f00f5534949be373cc6fecee9a09979b2d3ed328
1599053@uab.cat <ycordero@ui02.pic.es>
2025-03-13 14:11:09 +0100

    Adding Mask R-CNN code

--- week2/src/detectron/mask_rcnn.py
+++ week2/src/detectron/mask_rcnn.py
@@ -9,6 +9,10 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 
 from week2.src.detectron.dataset import CustomKittiMotsDataset
+from week2.src.detectron.trainer import CustomTrainer
+
+# To test if it works
+from detectron2.engine import DefaultTrainer
 
 
 class MaskRCNN:
@@ -151,6 +155,86 @@ class MaskRCNN:
         print("Running inference on the test dataset...")
         return inference_on_dataset(predictor.model, val_loader, evaluator)
     
-    def train_model(self):
-        pass
+    def train_model(self, data_dir: str, dataset_name: str = "kitti-mots", num_classes: int = 2, output_dir: str = "./output/train", **kwargs) -> dict:
+        """Train a model to a given dataset.
+
+        Args:
+            data_dir (str): Dataset directory.
+            dataset_name (str, optional): Dataset name. Defaults to "kitti-mots".
+            num_classes (int, optional): Number of classes in the dataset. Defaults to 2.
+            output_dir (str, optional): Output directory for the training process. Defaults to "./output/train".
+            **kwargs: Additional arguments for the training process.
+
+        Returns:
+            dict: Results of the training process.
+        """
+        # Set input mask as bitmask
+        self.cfg.INPUT.MASK_FORMAT = "bitmask"
+        
+        # Register dataset and metadata for training and validation
+        try:
+            for split in ["train", "val"]:
+                DatasetCatalog.register(dataset_name + "_" + split, lambda: CustomKittiMotsDataset(data_dir, use_coco_ids=False, split=split))
+                MetadataCatalog.get(dataset_name + "_" + split).set(thing_classes=["0", "1"])
+        except AssertionError:
+            print("Dataset already registered, continuing...")
+        
+        # Model parameters
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = kwargs.get("batch_size_per_image", 128) # 128 is faster and enough for this dataset
+        self.cfg.MODEL.BACKBONE.FREEZE_AT = 5 if kwargs.get("freeze_backbone", False) else 0
+        
+        # Datasets parameters (train and test)
+        self.cfg.DATASETS.TRAIN = (dataset_name + "_train",)
+        self.cfg.DATASETS.TEST = (dataset_name + "_val",)
+
+        # Solver parameters (optimizer)
+        lr_scheduler = kwargs.get("lr_scheduler", "WarmupMultiStepLR")
+        assert lr_scheduler in ["WarmupMultiStepLR", "WarmupCosineLR"], "LR scheduler must be WarmupMultiStepLR or WarmupCosineLR"
+        self.cfg.SOLVER.LR_SCHEDULER_NAME = lr_scheduler # Learning rate scheduler
+        self.cfg.SOLVER.IMS_PER_BATCH = kwargs.get("batch_size", 8) # Batch size
+        print(f"BATCH SIZE: {self.cfg.SOLVER.IMS_PER_BATCH}")
+        self.cfg.SOLVER.BASE_LR = kwargs.get("base_lr", 0.001)  # Learning rate
+        self.cfg.SOLVER.MOMENTUM = kwargs.get("momentum", 0.9)  # Momentum
+        self.cfg.SOLVER.NESTEROV = kwargs.get("nesterov", False)  # Nesterov momentum
+        self.cfg.SOLVER.WEIGHT_DECAY = kwargs.get("weight_decay", 0.0001) # Weight decay
+        self.cfg.SOLVER.GAMMA = kwargs.get("gamma", 0.1) # Learning rate decay factor
+        self.cfg.SOLVER.CLIP_GRADIENTS.ENABLED = kwargs.get("clip_gradients", False) # Clip gradients
+        self.cfg.SOLVER.STEPS = kwargs.get("steps", (1000, 2000)) # Steps for learning rate decay
+
+        # Fixed parameters (do not change)
+        self.cfg.SOLVER.MAX_ITER = kwargs.get("max_iter", 3000)
+        self.cfg.SOLVER.CHECKPOINT_PERIOD = kwargs.get("checkpoint_period", 1000)
+
+        # Test parameters (evaluation)
+        self.cfg.TEST.EVAL_PERIOD = kwargs.get("eval_period", 1000)
+
+        # Output directory for training logs and checkpoints
+        self.cfg.OUTPUT_DIR = output_dir
+        os.makedirs(self.cfg.OUTPUT_DIR, exist_ok=True)
+
+        # Create trainer and start training
+        print("Starting training...")
+        trainer = CustomTrainer(self.cfg)
+        # trainer = DefaultTrainer(self.cfg)
+        trainer.resume_or_load(resume=False)
+        trainer.train()
+        
+        print("-" * 50)
+        print("Starting final evaluation on validation")
+        print("Creating predictor...")
+        eval_cfg = self.cfg.clone()
+        eval_cfg.MODEL.WEIGHTS = os.path.join(self.cfg.OUTPUT_DIR, "model_final.pth")
+        predictor = DefaultPredictor(eval_cfg)
+        
+        # Evaluate on the test dataset using COCO evaluator
+        print("Performing evaluation...")
+        validation_output_dir = os.path.join(output_dir, "final_validation")
+        os.makedirs(validation_output_dir, exist_ok=True)
+        evaluator = COCOEvaluator(dataset_name + "_val", eval_cfg, False, output_dir=validation_output_dir)
+        val_loader = build_detection_test_loader(eval_cfg, dataset_name + "_val")
+        
+        # Run inference and return results
+        print("Running inference on the test dataset...")
+        return inference_on_dataset(predictor.model, val_loader, evaluator)
     
---
+++ week2/src/detectron/run_optuna.py
@@ -0,0 +1,70 @@
+import argparse
+import optuna
+
+from week2.src.detectron.mask_rcnn import MaskRCNN
+from functools import partial
+
+
+def objective(trial, model_trainer: MaskRCNN, data_dir: str, dataset_name: str, output_dir: str, freeze_backbone: bool = False):
+    # Hyperparameters to optimize
+    hyperparams = {
+        # Model hyperparameters
+        "batch_size_per_image": trial.suggest_categorical("batch_size_per_image", [64, 128, 256, 512]),
+        "batch_size": trial.suggest_categorical("batch_size", [8, 16]),
+        
+        # Learning rate and scheduler
+        "base_lr": trial.suggest_float("base_lr", 1e-4, 1e-2, log=True),
+        "lr_scheduler": trial.suggest_categorical("lr_scheduler", ["WarmupMultiStepLR", "WarmupCosineLR"]),
+        "weight_decay": trial.suggest_float("weight_decay", 1e-5, 1e-3, log=True),
+        
+        # Whether to clip gradients or not
+        "clip_gradients": trial.suggest_categorical("clip_gradients", [True, False]),
+        #"clip_gradients": False,
+        
+        # Wether to freeze or not the backbone
+        "freeze_backbone": freeze_backbone
+    }
+    
+    # Train with current hyperparameters
+    output_dir = f"{output_dir}/trial_{trial.number}"
+    results = model_trainer.train_model(
+        data_dir=data_dir,
+        dataset_name=dataset_name,
+        output_dir=output_dir,
+        **hyperparams
+    )
+    
+    # Extract metrics (assuming COCO mAP as primary metric)
+    return results['segm']['AP']
+
+
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='KittiMots Dataset')
+    parser.add_argument('-d', '--data_dir', help="Path to validation dataset", required=True)
+    parser.add_argument('-c', '--config_file', default="COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml", help="Path to the model config yaml from model zoo.")
+    parser.add_argument('-w', '--weights_file', default="COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml", help="Path to the weights file or model zoo config yaml.")
+    parser.add_argument('-s', '--score_threshold', type=float, default=0.5, help="Score threshold for predictions.")
+    parser.add_argument('-o', '--output_dir', help="Output directory for the model", default=None, required=True)
+    parser.add_argument('-fb', '--freeze_backbone', help="Wether to freeze or not the backbone", action="store_true")
+    parser.add_argument('--num_workers', required=False, default=4, type=int, help="Number of workers to load dataset.")
+    args = parser.parse_args()
+    
+    # Get the arguments from CLI
+    data_dir = args.data_dir
+    config_file = args.config_file
+    weights_file = args.weights_file
+    score_threshold = args.score_threshold
+    num_workers = args.num_workers
+    output_dir = args.output_dir
+    freeze_backbone = args.freeze_backbone
+    
+    # Get the model
+    model = MaskRCNN(config_file, weights_file, score_threshold, num_workers)
+    
+    # Create study and optimize
+    study = optuna.create_study(direction="maximize")
+    study.optimize(
+        partial(objective, model_trainer=model, data_dir=data_dir, dataset_name="kitti-mots", output_dir=output_dir, freeze_backbone=freeze_backbone),
+        n_trials=20
+    )
